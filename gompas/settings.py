ae1959632b6914bb20234d836c5d53a10d3355eb
gloweean <gloweean@gmail.com>
2020-03-16 20:06:50 +1100

    create template folder and add app vegetable

--- gompas/settings.py
+++ gompas/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user',
+    'vegetable',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ vegetable/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Vegitable(models.Model):
+    name_ko = models.CharField(max_length=100, blank=True, null=True, verbose_name='야채 이름(한글)')
+    name_en = models.CharField(max_length=100, blank=True, null=True, verbose_name='야채 이름(영어)')
+    unit = models.CharField(max_length=100, blank=True, null=True, verbose_name='주요 단위')
+    sub_unit = models.CharField(max_length=100, blank=True, null=True, verbose_name='보조 단위')
+    sub_unit2 = models.CharField(max_length=100, blank=True, null=True, verbose_name='보조 단위2')
