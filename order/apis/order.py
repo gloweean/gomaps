0f995bfcd8d6c84496e9b47f77ae4a396f9bceeb
gloweean <gloweean@gmail.com>
2020-03-30 17:38:56 +1100

    ADD permission PUT, Modify order API(Total order update part)

--- order/apis/order.py
+++ order/apis/order.py
@@ -51,47 +51,26 @@ class OrderListCreateView(generics.ListCreateAPIView):
                     quantity_ea=0,
                 )
             
-            instance = TotalOrder.objects.get(vege_name_id=target_vege_id, deliver_date=deliver_date)
-            if order_unit is "CTN":
-                existed_value = instance.quantity_ctn
-                instance.quantity_ctn = existed_value + order_quantity
-            elif order_unit is "BAG":
-                existed_value = instance.quantity_bag
-                instance.quantity_bag = existed_value + order_quantity
-            elif order_unit is "KG":
-                existed_value = instance.quantity_kg
-                instance.quantity_kg = existed_value + order_quantity
-            else:
-                existed_value = instance.quantity_ea
-                instance.quantity_ea = existed_value + order_quantity
-
-            instance.save()
-            serializer.save(
-                operator=operator,
-                vegetable_name=target_vege,
-                customer=target_customer,
-            )
+        instance = TotalOrder.objects.get(vege_name_id=target_vege_id, deliver_date=deliver_date)
+        if order_unit in ["CTN", ]:
+            existed_value = instance.quantity_ctn
+            instance.quantity_ctn = existed_value + order_quantity
+        elif order_unit in ["BAG", ]:
+            existed_value = instance.quantity_bag
+            instance.quantity_bag = existed_value + order_quantity
+        elif order_unit in ["KG", ]:
+            existed_value = instance.quantity_kg
+            instance.quantity_kg = existed_value + order_quantity
         else:
-            instance = TotalOrder.objects.get(vege_name_id=target_vege_id, deliver_date=deliver_date)
-            if order_unit is "CTN":
-                existed_value = instance.quantity_ctn
-                instance.quantity_ctn = existed_value + order_quantity
-            elif order_unit is "BAG":
-                existed_value = instance.quantity_bag
-                instance.quantity_bag = existed_value + order_quantity
-            elif order_unit is "KG":
-                existed_value = instance.quantity_kg
-                instance.quantity_kg = existed_value + order_quantity
-            else:
-                existed_value = instance.quantity_ea
-                instance.quantity_ea = existed_value + order_quantity
-            
-            instance.save()
-            serializer.save(
-                operator=operator,
-                vegetable_name=target_vege,
-                customer=target_customer,
-            )
+            existed_value = instance.quantity_ea
+            instance.quantity_ea = existed_value + order_quantity
+        
+        instance.save()
+        serializer.save(
+            operator=operator,
+            vegetable_name=target_vege,
+            customer=target_customer,
+        )
 
 
 class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -113,7 +92,109 @@ class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         return user.order_set.all()
     
     def perform_update(self, serializer):
-        serializer.save()
+        # valid test 추가
         
+        # Total Order가 받는 before_order의 영향을 역으로 제거
+        order_id = self.kwargs["id"]
+        before_order = Order.objects.get(id=order_id)
+        
+        deliver_date_before = before_order.deliver_date
+        vege_before = before_order.vegetable_name  # instance
+        quantity_brfore = before_order.order_quantity
+        unit_before = before_order.order_unit
+        
+        instance = TotalOrder.objects.get(vege_name=vege_before, deliver_date=deliver_date_before)
+        if unit_before in ["CTN", ]:
+            existed_value = instance.quantity_ctn
+            instance.quantity_ctn = existed_value - quantity_brfore
+        elif unit_before in ["BAG", ]:
+            existed_value = instance.quantity_bag
+            instance.quantity_bag = existed_value - quantity_brfore
+        elif unit_before in ["KG", ]:
+            existed_value = instance.quantity_kg
+            instance.quantity_kg = existed_value - quantity_brfore
+        else:
+            existed_value = instance.quantity_ea
+            instance.quantity_ea = existed_value - quantity_brfore
+        instance.save()
+        
+        # 새로운 order의 영향을 재계산 후 Total Order 입력
+        deliver_date = serializer.validated_data["deliver_date"]
+        target_vege_id = serializer._kwargs["data"]["vegetable_name"]
+        customer_id = serializer._kwargs["data"]["customer"]
+
+        operator = self.request.auth.user  # instance
+        target_vege = Vegetable.objects.get(id=target_vege_id)  # instance
+        target_customer = Customer.objects.get(id=customer_id)  # instance
+
+        order_unit = serializer.validated_data["order_unit"]
+        order_quantity = serializer.validated_data["order_quantity"]
+        
+        if TotalOrder.objects.filter(deliver_date=deliver_date).last() is None:
+            vegetable_list = Vegetable.objects.all()
+            for vegetable in vegetable_list:
+                TotalOrder.objects.create(
+                    deliver_date=deliver_date,
+                    vege_name=vegetable,
+                    quantity_bag=0,
+                    quantity_ctn=0,
+                    quantity_kg=0,
+                    quantity_ea=0,
+                )
+                
+        instance = TotalOrder.objects.get(vege_name_id=target_vege_id, deliver_date=deliver_date)
+        if order_unit in ["CTN", ]:
+            existed_value = instance.quantity_ctn
+            instance.quantity_ctn = existed_value + order_quantity
+        elif order_unit in ["BAG", ]:
+            existed_value = instance.quantity_bag
+            instance.quantity_bag = existed_value + order_quantity
+        elif order_unit in ["KG", ]:
+            existed_value = instance.quantity_kg
+            instance.quantity_kg = existed_value + order_quantity
+        else:
+            existed_value = instance.quantity_ea
+            instance.quantity_ea = existed_value + order_quantity
+        instance.save()
+        
+        # 주문 정보 수정
+        try:
+            serializer.save(
+                operator=operator,
+                vegetable_name=target_vege,
+                customer=target_customer,
+            )
+        except Exception as e:  # Error 시 다시 되돌리기
+            instance = TotalOrder.objects.get(vege_name=vege_before, deliver_date=deliver_date_before)
+            if unit_before in ["CTN", ]:
+                existed_value = instance.quantity_ctn
+                instance.quantity_ctn = existed_value + quantity_brfore
+            elif unit_before in ["BAG", ]:
+                existed_value = instance.quantity_bag
+                instance.quantity_bag = existed_value + quantity_brfore
+            elif unit_before in ["KG", ]:
+                existed_value = instance.quantity_kg
+                instance.quantity_kg = existed_value + quantity_brfore
+            else:
+                existed_value = instance.quantity_ea
+                instance.quantity_ea = existed_value + quantity_brfore
+            instance.save()
+
+            instance = TotalOrder.objects.get(vege_name_id=target_vege_id, deliver_date=deliver_date)
+            if order_unit in ["CTN", ]:
+                existed_value = instance.quantity_ctn
+                instance.quantity_ctn = existed_value - order_quantity
+            elif order_unit in ["BAG", ]:
+                existed_value = instance.quantity_bag
+                instance.quantity_bag = existed_value - order_quantity
+            elif order_unit in ["KG", ]:
+                existed_value = instance.quantity_kg
+                instance.quantity_kg = existed_value - order_quantity
+            else:
+                existed_value = instance.quantity_ea
+                instance.quantity_ea = existed_value - order_quantity
+            instance.save()
+            return e
+
     def perform_destroy(self, instance):
         instance.delete()
--- utils/permissions/__init__.py
+++ utils/permissions/__init__.py
@@ -6,14 +6,14 @@ class IsOrderOwner(permissions.BasePermission):
     유저가 변경하려는 Order가 자기 자신의 Order인지 검사
     """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS or "PUT":
             return obj.operator == request.user
         return False
 
 
 class ObjectIsRequestUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS or "PUT":
             return obj == request.user
         else:
             return False
@@ -24,6 +24,6 @@ class IsThisCustomerOperator(permissions.BasePermission):
     유저가 변경하려는 Customer가 해당 Operator(User)의 Customer인지 검사
     """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS or "PUT":
             return obj.operator == request.user
         return False
