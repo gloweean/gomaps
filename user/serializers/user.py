71e2741e432b972d79867ec054d8b6b9d529bff1
gloweean <gloweean@gmail.com>
2020-05-24 19:13:29 +1000

    User serializer modified

--- user/serializers/user.py
+++ user/serializers/user.py
@@ -15,6 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'username',
+            'store_name',
             'email',
             'gender',
             'phone_num',
@@ -35,6 +36,7 @@ class UserCreateSerializer(serializers.Serializer):
     username = serializers.CharField(allow_blank=False, allow_null=False)
     email = serializers.EmailField()
     name = serializers.CharField(allow_blank=False, allow_null=False)
+    store_name = serializers.CharField(allow_blank=False, allow_null=False)
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
 
@@ -51,11 +53,13 @@ class UserCreateSerializer(serializers.Serializer):
     def save(self, *args, **kwargs):
         username = self.validated_data.get('username', '')
         name = self.validated_data.get('name', '')
+        store_name = self.validated_data.get('store_name', '')
         email = self.validated_data.get('email', '')
         password = self.validated_data.get('password1', '')
         user = User.object.create_user(
             username=username,
             name=name,
+            store_name=store_name,
             email=email,
             password=password,
         )
